2cd8c373662b6a19fcbff1f64deb546d6c1e64d0
HologramPeter <hologrampeter@gmail.com>
2024-08-25 20:07:19 +1000

    fixed crashing issue, modified json protocol and display colors

--- telemetry/ui/src/live_value.tsx
+++ telemetry/ui/src/live_value.tsx
@@ -1,15 +1,42 @@
 import "./App.css";
 
-interface TemperatureProps {
-  temp: number;
+interface LastTemperatureProps {
+  isValid: boolean;
+  lastValue: number;
 }
 
-function LiveValue({ temp }: TemperatureProps) {
-  let valueColour = "white";
+function LiveValue({ isValid, lastValue }: LastTemperatureProps) {
+  if (lastValue === -500) {
+    return (
+      <header className="live-value" style={{ color: "white" }}>
+        {"Await Connection..."}
+      </header>
+    );
+  }
+
+  var valueColour = "white";
+  if (lastValue < 20 || lastValue > 80) {
+    valueColour = "red";
+  }else if (lastValue < 25 || lastValue > 75) {
+    valueColour = "yellow";
+  }else{
+    valueColour = "green";
+  }
+
+  //to precision 3 keep 3 decimal place trailing zeros
+
+  if (!isValid) {
+    return (
+      <header className="live-value invalid" style={{ color: valueColour }}>
+        {`${lastValue.toPrecision(3)}°C`}
+        <div className="remarks">Standby...</div>
+      </header>
+    );
+  }
 
   return (
     <header className="live-value" style={{ color: valueColour }}>
-      {`${temp.toPrecision(3)}°C`}
+      {`${lastValue.toPrecision(3)}°C`}
     </header>
   );
 }
